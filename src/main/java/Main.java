0b3665e4bd903041980d185a3cf66750f50ec0d9
Danilamaikl <Danilamaikl@yandex.ru>
2026-03-08 18:16:36 +0300

    Добавлена реальная функциональность

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,14 @@ import model.Food;
 import model.Meat;
 import model.Apple;
 import service.ShoppingCart;
+import model.constants.Colour;
 
 public class Main {
     public static void main(String[] args) {
 
         Food meat = new Meat(5, 100);
-        Food redApples = new Apple(10, 50, model.constants.Colour.RED);
-        Food greenApples = new Apple(8, 60, model.constants.Colour.GREEN);
+        Food redApples = new Apple(10, 50, Colour.RED);
+        Food greenApples = new Apple(8, 60, Colour.GREEN);
 
         Food[] products = {meat, redApples, greenApples};
         ShoppingCart cart = new ShoppingCart(products);
